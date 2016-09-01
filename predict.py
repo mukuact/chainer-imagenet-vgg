178471a5865e32f207e4bd0e8e3d4e0a6648815e
Muku Takeda <m-takeda@jsk.imi.i.u-tokyo.ac.jp>
2016-09-01 13:09:11 +0900

    first commit

--- predict.py
+++ predict.py
@@ -11,6 +11,21 @@ from chainer import cuda
 from chainer import serializers
 from chainer import Variable
 
+import matplotlib.pyplot as plt
+
+
+def plotting(features):
+    fig = plt.figure()
+    num = 512
+    for i in range(num):
+        axi = fig.add_subplot(16, 32, i)
+        axi.imshow(features[i])
+        axi.tick_params(labelbottom='off')
+        axi.tick_params(labelleft='off')
+
+    return fig
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--gpu', type=int, default=-1)
@@ -26,7 +41,7 @@ if __name__ == '__main__':
     vgg = VGGNet()
     serializers.load_hdf5('VGG.model', vgg)
     if args.gpu >= 0:
-        cuda.get_device(args.gpu).user()
+        cuda.get_device(args.gpu).use()
         vgg.to_gpu()
         img = cuda.cupy.asarray(img, dtype=np.float32)
 
@@ -34,9 +49,12 @@ if __name__ == '__main__':
 
     if args.gpu >= 0:
         pred = cuda.to_cpu(pred.data)
+        feature = cuda.to_cpu(vgg.last_feature.data)
     else:
         pred = pred.data
+        feature = vgg.last_feature.data
 
+    fig = plotting(feature[0])
     words = open('data/synset_words.txt').readlines()
     words = [(w[0], ' '.join(w[1:])) for w in [w.split() for w in words]]
     words = np.asarray(words)
@@ -45,3 +63,6 @@ if __name__ == '__main__':
     probs = np.sort(pred)[0][::-1][:5]
     for w, p in zip(words[top5], probs):
         print('{}\tprobability:{}'.format(w, p))
+
+    fig.show()
+    hoge = input()
